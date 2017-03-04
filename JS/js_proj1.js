b93399437a919478bbb4dd66aed7427426543738
mho8 <mho8@pitt.edu>
2017-03-03 22:55:14 -0500

    Add files via upload

---
+++ JS/js_proj1.js
@@ -0,0 +1,11 @@
+ document.getElementById("homeButton").onclick = function () {
+        location.href = "root_proj1.html";
+    };
+	
+ document.getElementById("productsButton").onclick = function () {
+        location.href = "products_proj1.html";
+    };
+	
+ document.getElementById("cartButton").onclick = function () {
+        location.href = "cart_proj1.html";
+    };
